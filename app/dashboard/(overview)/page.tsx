74076cea22b1d4e604baecfdfb0e6bbaadb5bc09
Monish Agrawal <monish.21910923@viit.ac.in>
2024-03-20 20:16:19 +0530

    complete auth and metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,7 +6,12 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
   const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices}= await fetchCardData();
   return (
